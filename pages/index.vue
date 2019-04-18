f151b12670a390650fc6a6722dc79d42b65285e4
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2019-04-18 17:21:57 -0500

    Se configura axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,31 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        api-games
-      </h1>
-      <h2 class="subtitle">
-        My groovy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <h1>Inicio</h1>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+
+axios.defaults.headers.common['user-key'] = 'aaa';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 export default {
-  components: {
-    Logo
+  data(){
+    return {
+      games: [
+
+      ]
+    }
+  },
+  asyncData() {
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+    return axios.get(`${cors}https://api-v3.igdb.com/games/?fields=name,genres.name,cover.url,popularity&order=popularity:desc&expand=genres`)
+      .then(res => {
+        return {
+          games:res.data
+        }
+      })
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
